2617b9a82435fb94aa014c8f4360bb60ef006689
chulbulikushi07 <chulbulikushi07@gmail.com>
2026-03-20 19:23:46 +0530

    Added a fps detector

--- openCV.py
+++ openCV.py
@@ -1,5 +1,7 @@
 import cv2
 from ultralytics import YOLO
+# we measure fps = 1/(time taken to process one frame)
+import time 
 
 #Load Yolo model
 #Yolov8 = model version, n = nano(small and fast), .pt = pytorch model file 
@@ -13,7 +15,12 @@ if not cam.isOpened():
     print("Error: Could not open webcam.")
     exit()
 
+prev_time = 0
+
 while True:
+    #time.time() gives current time in seconds.
+    start_time = time.time()
+    
     #this grabs one frame from the camera and returns it with a boolean value(t/f)
     #frame is a NumPy array of shape (height, width, channels)
     #channels = 3(BGR)
@@ -28,6 +35,33 @@ while True:
 
     # Draw bounding boxes + Draw labels + Draws confidence scores.
     annotated_frame = results[0].plot()
+
+    #calculating fps ,end time - start time = total time for one loop iteration.
+    end_time = time.time()
+    fps = 1/(end_time - start_time)
+
+    #putting fps text on screen
+    #This draws text on the image.
+
+    #Parameters:
+
+    #Image
+    #Text
+    #Position (x, y)
+    #Font
+    #Size
+    #Color (B, G, R)
+    #Thickness
+    
+    cv2.putText(
+        annotated_frame,
+        f"FPS: {int(fps)}",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
 
     # Display the processed frame in a window
     #first arguement shows the windows title and second arguement is the image array
